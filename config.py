c91636dfda450b65cb472be81eb1b79b3ca38c72
jack <oncwnuGXaCad6iKHBiBOYdIhHD0U@git.weixin.qq.com>
2023-02-20 23:06:20 +0800

    update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@
 
 import openai
 # Use the openai library to set the API key
-openai.api_key = "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"
+# openai.api_key = "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"
+openai.api_key = "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"
 
 COMPLETIONS_MODEL = "text-davinci-003"
 image_url = "https://p1-tt.byteimg.com/origin/pgc-image/fe41801208fa40d394352e0df71e9202?from=pc"
